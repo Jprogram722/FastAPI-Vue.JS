14dbc626a7bc1de9c11a3be3013830dac9ba8dd6
Jared <PJared722@gmail.com>
2023-08-18 14:37:31 -0300

    made a seperate function in the helper.py file to handle categories

--- backend/crud.py
+++ backend/crud.py
@@ -1,10 +1,8 @@
 # this file contains all the controller function for the api
 
-import sqlalchemy.exc
 from sqlalchemy.orm import Session
 import models, shemas
-from helper import get_img_path
-import os
+from helper import get_img_path, check_uniqueness
 
 def get_products(db: Session, skip: int = 0, limit: int = 100) -> list[dict[any, any]]:
     """
@@ -21,24 +19,12 @@ def get_products(db: Session, skip: int = 0, limit: int = 100) -> list[dict[any,
     ).join(models.CategoryModel, models.ProductModel.category_id == models.CategoryModel.id).all()
 
     res = [dict(data._mapping) for data in data_db]
-
-    test = db.query(models.CategoryModel).all()
-    print(len(test))
-    for i, category in enumerate(test):
-        print(i, category.category_name)
-    
     return res
 
 def create_product_item(db: Session, item: shemas.ProductSchema) -> models.ProductModel:
     img_path = get_img_path(item.img_file)
 
-    for i, category in enumerate(db.query(models.CategoryModel).all()):
-        if (category.category_name == item.category):
-            db_category = db.query(models.CategoryModel).filter(models.CategoryModel.id == category.id).first()
-        elif (i == len(db.query(models.CategoryModel).all())):
-            db_category = models.CategoryModel(
-                category_name = item.category
-            )
+    db_category = check_uniqueness(item.category, db)
 
     db_product = models.ProductModel(
         name = item.name,
--- backend/helper.py
+++ backend/helper.py
@@ -1,5 +1,8 @@
 # this file will contain helper functions
 
+import models
+from sqlalchemy.orm import Session
+
 def get_img_path(img_file: str) -> str:
     """
     This function will take in a img file as a string and concatinate it will a folder path string
@@ -16,3 +19,19 @@ def get_img_path(img_file: str) -> str:
         img_path = img_file
 
     return img_path
+
+def check_uniqueness(category_name: str, db: Session) -> models.CategoryModel:
+    """
+    This function will take in a category name and a database session to check and see if a category has already been created
+    in the SQL database
+
+    Example:
+    >>> check_uniqueness("electronics", db)
+    >>> <models.CategoryModel at "some memory address">
+    """
+
+    for category in db.query(models.CategoryModel).all():
+        if (category_name == category.category_name):
+            return db.query(models.CategoryModel).filter(models.CategoryModel.id == category.id).first()
+    return models.CategoryModel(category_name = category_name)
+
